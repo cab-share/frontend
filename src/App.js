23af4376cc2d756d03a8ddd0e661cd961a9c27c7
abhishek <malviya.abhishek853@gmail.com>
2022-11-03 00:41:56 +0530

    normal routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 
 import { useState } from "react";
-import {  BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
+import {  BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import './App.css';
 import Map from './container/Map/Map.js'
 import SlotSelector from "./container/SlotSelector/SlotSelector";
 
 function App() {
+
   const [pickCoordinates, setPickCoordiantes ] = useState({lat :  12.971891, lng : 77.641151});
   const [dropCoordinates, setDropCoordiantes ] = useState({lat :  12.971891, lng : 77.641151});
-  
+
+
   return (
     <div className='App' >
       <div className='App-header'>
-        <HashRouter>
+        <Router>
           <Routes>
             <Route 
               path="/" 
@@ -32,7 +34,7 @@ function App() {
                 />} 
             />
           </Routes>
-        </HashRouter>
+        </Router>
       </div>
     </div>
   );
