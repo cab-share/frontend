8a37eb273434a5500ae721a5b0f1aafd45eafaa0
abhishek <malviya.abhishek853@gmail.com>
2022-11-20 18:43:27 +0530

    google code login

--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -2,17 +2,30 @@ import classes from './Login.module.css';
 import { LinkedInOAuth } from "../../service/linkedInOauth2";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faLinkedinIn, faGoogle} from '@fortawesome/free-brands-svg-icons'
-
 import { useGoogleLogin } from '@react-oauth/google';
-
 
 
 function Login(){
 
     let linkedInOAuth = new LinkedInOAuth("csrftoken", window.location.origin);
     const login = useGoogleLogin({
-        onSuccess: tokenResponse => console.log(tokenResponse),
+        flow: 'auth-code',
+        onSuccess: success
       });
+
+    async function  success(tokenResponse){
+        console.log("Token:- ", tokenResponse);
+        // setTimeout( async()=>{
+        //     const userInfo = await axios
+        //     .get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        //       headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+        //     })
+        //     .then(res => res.data);
+        //   console.log(userInfo);
+
+        // }, 5000 )
+
+    }
 
     return (
         <div className={classes["container"]} >
