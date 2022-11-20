bac0008015fd47ba30efe8e61bd075d47455c233
abhishek <malviya.abhishek853@gmail.com>
2022-11-20 18:14:12 +0530

    google frontend ready

--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -1,22 +1,29 @@
 import classes from './Login.module.css';
 import { LinkedInOAuth } from "../../service/linkedInOauth2";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
+import {faLinkedinIn, faGoogle} from '@fortawesome/free-brands-svg-icons'
+
+import { useGoogleLogin } from '@react-oauth/google';
+
 
 
 function Login(){
 
     let linkedInOAuth = new LinkedInOAuth("csrftoken", window.location.origin);
-
-    // console.log(linkedInOAuth.getUrl());
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+      });
 
     return (
         <div className={classes["container"]} >
-            <a  href={linkedInOAuth.getUrl()} className={classes["link"]}>                
-                <FontAwesomeIcon icon={faLinkedinIn} className={classes["font-awesome"]} />
+            <button className={classes["link"]}  onClick={()=>{login()}} >                
+                    <FontAwesomeIcon icon={faGoogle} className={classes["font-awesome"] + " " + classes["google"]} />
+                    <span> Login with Google </span>
+            </button>
+            <a className={classes["link"]}  href={linkedInOAuth.getUrl()} >                
+                <FontAwesomeIcon icon={faLinkedinIn} className={classes["font-awesome"] + " " + classes["linkedin"] } />
                 <span> Login with LinkedIn </span>
             </a>
-        
         </div>)
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import {Helmet} from "react-helmet";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <App />
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>My Title</title>
-      <link rel="canonical" href="http://mysite.com/example" />
-    </Helmet>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
