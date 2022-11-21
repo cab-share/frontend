959037b422369b30a7e6500d72eb34372d2ef165
abhishek <malviya.abhishek853@gmail.com>
2022-11-21 23:36:36 +0530

    external files removed

--- src/component/Icons/Icons.js
+++ src/component/Icons/Icons.js
@@ -1,18 +1,24 @@
 import classes from "./Icons.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlaneDeparture, faPlaneArrival , faYinYang } from '@fortawesome/free-solid-svg-icons';
 
 
-export function TaxiIcon({pramClass}){
-    return <span className={ pramClass + " " + classes["icon"] +   " material-symbols-outlined"}> local_taxi </span>
-}
+
+// export function TaxiIcon({pramClass}){
+//     return <span className={ pramClass + " " + classes["icon"] +   " material-symbols-outlined"}> local_taxi </span>
+// }
+
 
 export function FilghtTakeOff({pramClass}){
-    return <span className={ pramClass + " " + classes["icon"] +   " material-symbols-outlined"}> flight_takeoff </span>
+    return <span ><FontAwesomeIcon icon={faPlaneDeparture} className={"awd"} /></span>
+
 }
 
+
 export function FilghtLand({pramClass}){
-    return <span className={ pramClass + " " + classes["icon"] +   " material-symbols-outlined"}> flight_land </span>
+    return <span > <FontAwesomeIcon icon={faPlaneArrival} className={"awd"} /> </span>;
 }
 
 export function TwoPlane({pramClass}){
-    return <span className={ pramClass + " " + classes["icon"] +   " material-symbols-outlined"}> connecting_airports </span>
+    return <span className={classes["icon"]} > <FontAwesomeIcon icon={faYinYang} className={"awd"} /> </span>
 }
